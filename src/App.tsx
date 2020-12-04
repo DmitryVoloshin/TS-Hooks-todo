73af854522ef91842fd72fd00cf6942478eb6862
Dmitriy Voloshin <voloshincode@gmail.com>
2020-12-04 14:46:11 +0200

    connecting all component with each other

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 import './App.css';
 import Header from './components/header/header';
 import TodoForm from './components/todo-form/todo-form';
 
 const App: React.FC = ( ) =>{
+
+  const [todos,setTodos ] = useState([])
+
+  const addHandler = (title:string ) =>{
+      console.log('Add new todo',title)
+  }
+
   return(
     <>
       <Header/>
       <div className="">
-          <TodoForm/>
+          <TodoForm onAdd={addHandler}/>
       </div>
     </>
   )
--- src/components/todo-form/todo-form.tsx
+++ src/components/todo-form/todo-form.tsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import React,{useState} from 'react';
 
-const TodoForm: React.FC = ( ) =>{
+import './todo-form.css'
+
+interface TodoFormProps{
+    onAdd(title:string): void;
+
+}
+
+
+const TodoForm: React.FC<TodoFormProps> = (props) =>{
+
+    const [ title , setTitle] = useState<string>('');
+
+    const changeHandler = ( event: React.ChangeEvent<HTMLInputElement> ) =>{
+        setTitle(event.target.value)
+    }
+    const keyPressHandler = ( event: React.KeyboardEvent) =>{
+        if(event.key === 'Enter') {
+            props.onAdd(title)
+            setTitle('')
+        }
+    }
+   
+
     return(
         <div className="todo-form_block">
-            <input type="text" id="title"/>
-            <label htmlFor="title" className="active">
+              <label htmlFor="title" className="active">
                 What need to do today?
             </label>
+            <input 
+                onChange={changeHandler}
+                value={title} 
+                onKeyPress={keyPressHandler}
+                type="text" 
+                id="title" 
+                placeholder="what to do today?"
+            />
         </div>
     )
 }
